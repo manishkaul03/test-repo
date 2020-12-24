63ff2395dfb59a7d67ac06c3c3093c2f0118647c
Manish Kaul <manish.kaul@ectosense.com>
2020-12-24 19:32:38 +0530

    added batch file

---
+++ gitPushLocalChangesToRemote.bat
@@ -0,0 +1,16 @@
+@echo off
+set /p url-name="Please enter URL name: "
+set /p commit_message="Please enter commit message: "
+set /p branch_name="Please enter the name of active branch: "
+
+
+echo Helping you commit your local changes to the %url-name% github repo
+
+git checkout %branch_name%
+git fetch %url-name%
+git status
+git add -A
+git commit -m "%commit_message%"
+git push %url-name% %branch_name%
+
+pause > nul
