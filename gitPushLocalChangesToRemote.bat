995cd7eee7969adfb6e03642894a0b1ae2699fdd
Manish Kaul <manish.kaul@ectosense.com>
2021-01-22 16:29:42 +0530

    dont know

--- gitPushLocalChangesToRemote.bat
+++ gitPushLocalChangesToRemote.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 set /p url-name="Please enter URL name: "
 set /p commit_message="Please enter commit message: "
 set /p branch_name="Please enter the name of active branch: "
@@ -6,6 +6,7 @@ set /p branch_name="Please enter the name of active branch: "
 
 echo Helping you commit your local changes to the %url-name% github repo
 
+@echo on
 git checkout %branch_name%
 git fetch %url-name%
 git status
